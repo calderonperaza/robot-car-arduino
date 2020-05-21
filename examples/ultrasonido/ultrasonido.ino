3318936d55e59b347b2d0f63ad914c002ad40e67
andreacuellar2 <cuellarandrea10@gmail.com>
2020-05-21 11:59:44 -0500

    clase Carro
    Agregando clase carro a robot-car-arduino.h y agregando funcionalidad a la clase robot-car-arduino.cpp. Cambios en ejemplo de caminando.

--- examples/ultrasonido/ultrasonido.ino
+++ examples/ultrasonido/ultrasonido.ino
@@ -6,15 +6,15 @@ byte pinEcho=9;
 byte pinTrigger=10;
 
 
-Ultrasonido sensor; //declaracion del objeto
+Ultrasonido sensor2; //se esta invocando el constructor por defecto, declaracion del objeto
 
 void setup(){
     Serial.begin(9600);
-    sensor= Ultrasonido(&pinTrigger, &pinEcho); //Se Invoca al constructor p/ configurar valores
+    sensor2= Ultrasonido(&pinTrigger, &pinEcho); //Se Invoca al constructor para la configuracion de los valores
 }
 
 void loop(){
     
-    Serial.println(sensor.medirCM()); //llamado al sensor para imprimir la distancia en centimetros
+    Serial.println(sensor2.medirCM()); //llamado a sensor 2 para imprimir la distancia en CM
     delay(5000);
 }
