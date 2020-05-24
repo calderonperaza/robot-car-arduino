d9473141e9c1a665680a772b43ce57d5947d268a
Juan Pleitez <juanpc13lolol@gmail.com>
2020-05-23 21:56:54 -0600

    Carro Automata para usar en carrito

--- examples/Motor/ejemplo1caminando/ejemplo1caminando.ino
+++ examples/Motor/ejemplo1caminando/ejemplo1caminando.ino
@@ -1,24 +1,22 @@
 #include <robot-car-arduino.h>
-Carro m1;
+Carro carro;
 
-
-void setup() {
+void setup(){
   // no se necesita codigo porque el constructor de la clase configura todo
-
 }
 
-void loop() {
+void loop(){
   // put your main code here, to run repeatedly:
-m1.mover(10,10); //moverse hacia adelante
-delay(3000);
-m1.mover(-10,-10); //moverse hacia atras
-delay(3000);
-m1.mover(-10,0);  //se mueve solo la llanta izquierda hacia atras
-delay(3000);
-m1.mover(0,-10);//se mueve solo la llanta derecha hacia atras
-delay(3000);
-m1.mover(-10,-10); //ambas llantas hacia atras
-delay(3000);
-m1.mover(-5,-5); //ambas llantas hacia atras con una potencia de 125
-delay(3000);
+  carro.mover(10, 10); //moverse hacia adelante
+  delay(3000);
+  carro.mover(-10, -10); //moverse hacia atras
+  delay(3000);
+  carro.mover(-10, 0); //se mueve solo la llanta izquierda hacia atras
+  delay(3000);
+  carro.mover(0, -10); //se mueve solo la llanta derecha hacia atras
+  delay(3000);
+  carro.mover(-10, -10); //ambas llantas hacia atras
+  delay(3000);
+  carro.mover(-5, -5); //ambas llantas hacia atras con una potencia de 125
+  delay(3000);
 }
